b84e313b5b60e5df6d0137e6a663ddc9761049dc
Mario Pabon <me@mariopabon.com>
2017-02-17 17:33:29 -0500

    First commit :tada:

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Webpack Example
+
+Stay tuned...
---
+++ app/index.js
@@ -0,0 +1,5 @@
+import Hello from '../app/components/hello'
+import React from 'react'
+import {render} from 'react-dom'
+
+render(<Hello />, document.getElementById('app'))
